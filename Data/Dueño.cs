924be7f8d2af9ee65abab9e323f4963eb1062b9f
Pablo-fgr <pablito03121@hotmail.es>
2025-11-10 12:40:59 -0300

    Add project files.

---
+++ Data/Dueño.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Veterinaria.Data
+{
+    public class Dueño
+    {
+        public int IdDueño { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        public string NombreCompleto { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El DNI es obligatorio.")]
+        [RegularExpression(@"^[0-9]{8}$", ErrorMessage = "El DNI debe tener 8 dígitos.")]
+        public int Dni { get; set; }
+
+        [Required(ErrorMessage = "La fecha de nacimiento es obligatoria.")]
+        public DateTime? FechaNacimiento { get; set; }
+
+        [Required(ErrorMessage = "El domicilio es obligatorio.")]
+        public string Domicilio { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El sexo es obligatorio.")]
+        public string Sexo { get; set; } = string.Empty;
+
+    }
+}
---
+++ Service/RenaperService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Veterinaria.Data;
+
+namespace Veterinaria.Service
+{
+    public class RenaperService
+    {
+        private readonly HttpClient _httpClient;
+        private const string apiUrl = "";
+
+        public RenaperService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        //public async Task<Dueño> GetPersonaByDni(int dni)
+        //{
+        //    string url = $"{apiUrl}{dni}";
+
+        //    try
+        //    {
+        //        var result = await _httpClient. buscar que metodo usan <Dueño>(url);
+        //        return result;
+        //    }
+        //    catch(HttpRequestException ex)
+        //    {
+        //        Console.WriteLine($"Error");
+        //        return null;
+        //    }
+        //}
+    }
+}
